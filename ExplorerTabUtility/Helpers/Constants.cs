9fb70e2eb1ede908ac1d6459ed486a34dce6fed0
mclvlc <89335488+mclvlc@users.noreply.github.com>
2025-08-10 10:25:42 +0800

    Add files via upload
    汉化文件上传

--- ExplorerTabUtility/Helpers/Constants.cs
+++ ExplorerTabUtility/Helpers/Constants.cs
@@ -4,10 +4,10 @@ internal static class Constants
 {
     internal const string AppName = "ExplorerTabUtility";
     internal const string MutexId = $"__{AppName}Hook__Mutex";
-    internal const string NotifyIconText = "Explorer Tab Utility: Force new windows to tabs.";
+    internal const string NotifyIconText = "资源管理器标签页工具：强制将新窗口转为标签页。";
     internal const string SettingsFileName = "settings.json";
     internal const string HotKeyProfilesFileName = "HotKeyProfiles.json";
-    internal const string JsonFileFilter = "JSON files (*.json)|*.json|All Files|*.*";
+    internal const string JsonFileFilter = "JSON 文件 (*.json)|*.json|所有文件|*.*";
     internal const string UpdateUrl = "https://api.github.com/repos/w4po/ExplorerTabUtility/releases/latest";
-    internal const string DefaultHotKeyProfiles = "[{\"Name\":\"Home\",\"HotKeys\":[91,69],\"Scope\":0,\"Action\":0,\"Path\":\"\",\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0},{\"Name\":\"Duplicate\",\"HotKeys\":[17,68],\"Scope\":1,\"Action\":1,\"Path\":null,\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0},{\"Name\":\"ReopenClosed\",\"HotKeys\":[16,17,84],\"Scope\":1,\"Action\":2,\"Path\":null,\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0}]";
+    internal const string DefaultHotKeyProfiles = "[{\"Name\":\"主页\",\"HotKeys\":[91,69],\"Scope\":0,\"Action\":0,\"Path\":\"\",\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0},{\"Name\":\"复制标签页\",\"HotKeys\":[17,68],\"Scope\":1,\"Action\":1,\"Path\":null,\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0},{\"Name\":\"重新打开关闭的标签页\",\"HotKeys\":[16,17,84],\"Scope\":1,\"Action\":2,\"Path\":null,\"IsHandled\":true,\"IsEnabled\":true,\"Delay\":0}]";
 }
